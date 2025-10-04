fdde60e61a1797f2283db5f5df166de52c6c3b86
prabhuwebdev <h.prabhu2dev@gmail.com>
2025-10-04 11:58:30 +0530

    Fifth Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'   // ✅ Bootstrap 5 CSS only
+
 import './index.css'
 import App from './App.jsx'
 
